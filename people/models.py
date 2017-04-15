8fa56731abcfa38d82d12dbea5a59ef3b5c4516c
woodj22 <joe.wood@bbc.co.uk>
2017-04-15 16:43:02 +0100

    Create migrations and data fields

--- people/models.py
+++ people/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    sam_account_name    = models.CharField(max_length=200, unique=True)
+    display_name        = models.CharField(max_length=200, null=True)
+    is_manager          = models.NullBooleanField()
+    photo_number        = models.IntegerField(null=True)
+
+
+class Leave(models.Model):
+    total       = models.IntegerField()
+    days_left   = models.IntegerField()
+    person_id   = models.ForeignKey(Person, on_delete=models.CASCADE)
