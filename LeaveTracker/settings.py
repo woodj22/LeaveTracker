fb2c143434b32e5ac506e58817c36c650395468c
woodj22 <joe.wood@bbc.co.uk>
2017-04-15 16:00:26 +0100

    Create people module and routes

--- LeaveTracker/settings.py
+++ LeaveTracker/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'LeaveTracker.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.leave_tracker'),
     }
 }
 
