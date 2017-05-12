56688e3486a041c18d3b8c14b3548fcd07aeac9b
woodj22 <joe.wood@bbc.co.uk>
2017-05-12 10:00:43 +0100

    Add blank values to person model to allow for blank entries on import.

--- people/models.py
+++ people/models.py
@@ -3,13 +3,13 @@ from django.db import models
 class Person(models.Model):
     id                  = models.AutoField(primary_key=True)
     sam_account_name    = models.CharField(max_length=200, unique=True)
-    display_name        = models.CharField(max_length=200, null=True)
-    is_manager          = models.NullBooleanField()
-    photo_number        = models.BigIntegerField(null=True)
-    sur_name            = models.CharField(max_length=255)
-    given_name          = models.CharField(max_length=255)
-    department          = models.CharField(max_length=255)
-    division            = models.CharField(max_length=255)
+    display_name        = models.CharField(max_length=200, null=True, blank=True)
+    is_manager          = models.NullBooleanField(blank=True)
+    photo_number        = models.BigIntegerField(null=True, blank=True)
+    sur_name            = models.CharField(max_length=255, blank=True)
+    given_name          = models.CharField(max_length=255, blank=True)
+    department          = models.CharField(max_length=255, blank=True)
+    division            = models.CharField(max_length=255, blank=True)
     def __str__(self):
         return self.sam_account_name
 
@@ -19,11 +19,11 @@ class Person(models.Model):
             number = cls.__sanitizeCsvData(row)
             cls.objects.filter(sam_account_name=row['samAccountName']).update_or_create(
                 sam_account_name=row['samAccountName'], defaults={
-                    'display_name':row['displayName'],
-                    'sur_name':row['surname'],
-                    'given_name':row['givenName'],
-                    'department':row['department'],
-                    'division':row['division'],
+                    # 'display_name':row['displayName'],
+                    # 'sur_name':row['surname'],
+                    # 'given_name':row['givenName'],
+                    # 'department':row['department'],
+                    # 'division':row['division'],
                     'photo_number': number
                 }
             )
